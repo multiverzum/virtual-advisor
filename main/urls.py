b0ae3bc84f34e07766a4c4abde118bb6ca8a8dce
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-02-13 11:16:37 +0100

    login and register pages

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
-from .views import getHome
+from .views import getHome, getLogin, getRegister
 from django.urls import path
 
 urlpatterns = [
 
     path("", getHome, name='home'),
+    path("login", getLogin, name='login'),
+    path("register", getRegister, name='register'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 
 def getHome(request):
     return render(request, 'home.html')
+
+def getLogin(request):
+    return render(request, 'login.html')
+
+def getRegister(request):
+    return render(request, 'register.html')
